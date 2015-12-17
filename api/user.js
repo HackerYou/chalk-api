29880f70e9865554fdcd769a873ce8fe5c66a8d5
Ryan Christiani <ryan@ryanchristiani.com>
2015-12-17 17:26:19 -0500

    Lesson shoudl check against a string

--- api/user.js
+++ api/user.js
@@ -384,7 +384,7 @@ user.removeFavoriteLesson = (req,res) => {
 		let lessonIndex = ((userDoc) => {
 			let index;
 			userDoc.favorites[courseId].lessons.forEach((lesson,i) => {
-				if(lesson._id === lessonId) {
+				if(lesson._id === lessonId.toString()) {
 					index = i;
 				}
 			});
