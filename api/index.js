d9096f8154cd123237da6575001414922cfae4ae
Ryan Christiani <ryan@ryanchristiani.com>
2017-02-15 16:59:20 -0500

    Adding production db connect

--- api/index.js
+++ api/index.js
@@ -1,7 +1,13 @@
 'use strict';
 
 let mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/notes');
+let config = require('../config.js');
+if(process.env.NODE_ENV === 'production') {
+	mongoose.connect(`mongodb://${config.dbUser}:${config.dbPass}@localhost`);
+}
+else {
+	mongoose.connect('mongodb://localhost/notes');
+}
 mongoose.Promise = Promise;
 
 module.exports = {
